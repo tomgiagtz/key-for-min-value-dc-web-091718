383345cb167d36e52a99f6fab06c8f572ce7847c
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-09-04 16:46:29 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+
+	curr_min = 1.0 / 0.0 # use infinity so will replace with first val
+	min_key = nil
+	name_hash.each do |key, value| 
+		if value < curr_min
+			min_key = key
+			curr_min = value
+		end
+		
+	end
+	min_key
 end
